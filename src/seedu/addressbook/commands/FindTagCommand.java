88631216b3ec56ef7eaa48d9ecf1a099e943e4d5
louietyj <louietyj@gmail.com>
2016-09-28 02:15:15 +0800

    Add command word and message usage.

--- src/seedu/addressbook/commands/FindTagCommand.java
+++ src/seedu/addressbook/commands/FindTagCommand.java
@@ -1,6 +1,12 @@
 package seedu.addressbook.commands;
 
 public class FindTagCommand extends Command {
+	
+	public static final String COMMAND_WORD = "findtag";
+	
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" 
+            + "Lists all persons in the address book with the searched tag.\n\t"
+            + "Example: " + COMMAND_WORD + " owemoney";
 
 	@Override
 	public CommandResult execute() {
