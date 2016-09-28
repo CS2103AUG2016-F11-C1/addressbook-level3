e1a3375b3055f1adc729516ed1a2a3f1e4e2890f
Irvin Lim <irvinlimweiquan@gmail.com>
2016-09-28 15:15:57 +0800

    Added AddTagCommand stub

---
+++ src/seedu/addressbook/commands/AddTagCommand.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.commands;
+
+public class AddTagCommand extends Command {
+
+	@Override
+	public CommandResult execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
